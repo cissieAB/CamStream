fcb6902771ccab797cddc430821868c8f5d0e5f2
BraveChair <churley@jlab.org>
2024-07-11 10:11:19 -0400

    not needed

--- local_view/quickstart.py
+++ local_view/quickstart.py
@@ -8,4 +8,16 @@ picam2 = Picamera2()
 camera_running = False
 config = picam2.create_preview_configuration()
 picam2.confingure(config)
-#hi
+picam2.start()
+picam2.start_preview(Preview.QTGL)
+print("Enter 'q' to quit: ")
+command = input("Press 'm' to view possible modes or hit 'b' to go back").strip().lower()
+
+def main():
+    if command == 'q' or KeyboardInterrupt:
+        print("Exiting ")
+        picam2.stop_preview()
+        picam2.stop()
+
+if __name__ == "__main__":
+    main()
