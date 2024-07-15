8a16db0dc79d0646211a92f064ad7c454a7b7f00
BraveChair <churley@jlab.org>
2024-07-15 12:45:22 -0400

    testing custom import path

--- fov_adapt/blend_weight.py
+++ fov_adapt/blend_weight.py
@@ -1,5 +1,20 @@
 
 import cv2
+from picamera2 import Picamera2, Preview
+import pithermalcam as ptc
+import time
+from pprint import *
+import keyboard
+
+def _show_processed_image(ptc):
+        """Resize image window and display it"""
+        cv2.namedWindow('Thermal Image', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Thermal Image', ptc.image_width,ptc.image_height)
+        cv2.imshow('Thermal Image', ptc._image)
+
+        output_folder = '/home/pi/PiThermalCam/saved_snapshots/'
+
+    thermcam = pithermalcam(output_folder=output_folder) 
 
 def blend_images(picam2_image, thermal_image):
     """Blend two images"""
@@ -15,3 +30,15 @@ def blend_images(picam2_image, thermal_image):
     blended_image = cv2.addWeighted(thermal_image, alpha, picam2_image, beta, 0)
 
     return blended_image
+
+def main():
+    cv2.namedWindow("Combined_feed",cv2.WINDOW_NORMAL)
+    # using cv2.imshow() to display the image 
+    cv2.imshow('Display', blended_image) 
+    
+    # Waiting 0ms for user to press any key 
+    cv2.waitKey(0) 
+    
+    # Using cv2.destroyAllWindows() to destroy 
+    # all created windows open on screen 
+    cv2.destroyAllWindows() 
